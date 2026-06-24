474d7860d9b497254e5bdcc754e74a7150d2a7df
Gal Buki <jkl@torus.ch>
2026-06-24 17:57:01 +0300

    fix test setup

--- src/test/suite/connection.test.ts
+++ src/test/suite/connection.test.ts
@@ -2,12 +2,12 @@ import * as assert from 'assert';
 
 suite('Connection Layer', () => {
   test('encodeLength produces correct little-endian bytes', () => {
-    const { NetreplConnection } = require('../repl/connection');
+    const { NetreplConnection } = require('../../repl/connection');
     assert.ok(NetreplConnection);
   });
 
   test('ConnectionState enum has expected values', () => {
-    const { ConnectionState } = require('../repl/connection');
+    const { ConnectionState } = require('../../repl/connection');
     assert.strictEqual(ConnectionState.Disconnected, 'disconnected');
     assert.strictEqual(ConnectionState.Connecting, 'connecting');
     assert.strictEqual(ConnectionState.Connected, 'connected');
@@ -17,8 +17,8 @@ suite('Connection Layer', () => {
 
 suite('Session', () => {
   test('ReplSession manages history correctly', () => {
-    const { ReplSession } = require('../repl/session');
-    const { NetreplConnection } = require('../repl/connection');
+    const { ReplSession } = require('../../repl/session');
+    const { NetreplConnection } = require('../../repl/connection');
     const conn = new NetreplConnection();
     const session = new (ReplSession as any)(conn);
 
